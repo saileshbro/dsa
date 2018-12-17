1e03d543b1f3c61d1c20004389720821ca9e3404
Sailesh Dahal <saileshbro@gmail.com>
2018-12-17 22:30:56 +0545

    created methods push() pop() and peek()

---
+++ stack.cpp
@@ -0,0 +1,26 @@
+#include "singlyLinkedList.cpp"
+class Stack:public LinkedList{
+    public:
+    Stack():LinkedList(){}
+    void push(int data){
+        LinkedList::addBack(data);
+    }
+    void pop(){
+        LinkedList::removeBack();
+    }
+    void print(){
+        LinkedList::print();
+    }
+    int peek(){
+        return LinkedList::getBack();
+    }
+};
+int main(){
+    Stack stack;
+    stack.push(10);
+    stack.push(22);
+    stack.push(19);
+    stack.pop();
+    stack.push(13);
+    stack.print();
+}
